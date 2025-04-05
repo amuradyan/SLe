23b34ae67b757f34d371f93ae07b22038835033e
darthspectrum <muradianator@gmail.com>
2025-04-05 18:00:32 +0400

    :stop: playing stops

--- impl/shvi.test.js
+++ impl/shvi.test.js
@@ -5,6 +5,7 @@ import { encodeWAV, generatePCM } from "./sintez.js";
 const atom = Symbol.for;
 
 const environment = [
+  [atom("silence"), 0],
   [atom("C4"), 261.63],
   [atom("G4"), 392.0],
   [atom("F4"), 349.23],
@@ -22,17 +23,19 @@ Deno.test("Playing things", async (t) => {
   await t.step({
     name: "playing a C4 for two seconds",
     fn: async () => {
+      const outputFile = "C4-for-2-seconds.wav";
+
       const music = `
             (tone C4 2)
           `;
 
       const samples = run(music, environment);
 
-      encodeWAV(samples, "C4-for-2-seconds.wav");
+      encodeWAV(samples, outputFile);
 
       console.log("Playing generated WAV file...");
       const process = new Deno.Command("aplay", {
-        args: ["C4-for-2-seconds.wav"],
+        args: [outputFile],
         stdout: "inherit",
         stderr: "inherit",
       }).spawn();
@@ -46,22 +49,51 @@ Deno.test("Playing things", async (t) => {
     name:
       "playing an F4 for one second, a C4 for two seconds, and a G4 for one second",
     fn: async () => {
+      const outputFile = "F4-C4-G4-for-2-seconds.wav";
+
       const music = `
-                (sequence (tone F4 1) (tone C4 2) (tone G4 1))
-            `;
+                  (sequence (tone F4 1) (tone C4 2) (tone G4 1))
+              `;
 
       const samples = run(music, environment);
 
-      encodeWAV(samples, "F4-C4-G4-for-2-seconds.wav");
+      encodeWAV(samples, outputFile);
 
       console.log("Playing generated WAV file...");
       const process = new Deno.Command("aplay", {
-        args: ["F4-C4-G4-for-2-seconds.wav"],
+        args: [outputFile],
         stdout: "inherit",
         stderr: "inherit",
       }).spawn();
 
       await process.output();
     },
+    ignore: true,
+  });
+
+  await t.step({
+    name:
+      "playing an F4 for one second, a silence for one second, a C4 for two seconds, and a G4 for one second",
+    fn: async () => {
+      const outputFile = "F4-C4-G4-for-2-seconds.wav";
+
+      const music = `
+                  (sequence (tone F4 1) (tone silence 1) (tone C4 2) (tone G4 1))
+              `;
+
+      const samples = run(music, environment);
+
+      encodeWAV(samples, outputFile);
+
+      console.log("Playing generated WAV file...");
+      const process = new Deno.Command("aplay", {
+        args: [outputFile],
+        stdout: "inherit",
+        stderr: "inherit",
+      }).spawn();
+
+      await process.output();
+    },
+    ignore: true,
   });
 });
