933792cf082f04935940a9652050a8a66777aa55
darthspectrum <muradianator@gmail.com>
2025-04-05 15:55:44 +0400

    :accordion: playing music from tests

--- impl/shvi.test.js
+++ impl/shvi.test.js
@@ -1,19 +1,28 @@
 import { fail } from "@std/assert/fail";
+import { run } from "./slim.js";
 
-Deno.test("playing a C4 for two seconds", async (t) => {
+Deno.test("Playing things", async (t) => {
   await t.step({
-    name: "luminance",
-    fn: () => {
+    name: "playing a C4 for two seconds",
+    fn: async () => {
       const code = `
             (tone C4 2)
           `;
 
-      run(code);
+      //   run(code);
+
+      console.log("Playing generated WAV file...");
+      const process = new Deno.Command("aplay", {
+        args: ["C4-for-2-seconds.wav"],
+        stdout: "inherit",
+        stderr: "inherit",
+      }).spawn();
+
+      await process.output();
 
       fail(
         "This test is not implemented yet. Please implement it.",
       );
     },
-    ignore: true,
   });
 });
