47133ee5f8d4e4091d373a985da9f78ac07ce1c4
darthspectrum <muradianator@gmail.com>
2025-04-12 15:32:58 +0400

    :adhesive_bandaid: broken main

--- ts/main.ts
+++ ts/main.ts
@@ -2,19 +2,19 @@ export function add(a: number, b: number): number {
   return a + b;
 }
 
-log.debug("Add 2 + 3 =", add(2, 3));
+console.log("Add 2 + 3 =", add(2, 3));
 
-log.debug(Deno.readTextFileSync("../notes/DetailedPlan.md"));
+console.log(Deno.readTextFileSync("../notes/DetailedPlan.md"));
 
 const l: (string | number | [])[] = ["hello", 42, []];
 
 const [first, ...rest] = l;
 
-log.debug(first);
-log.debug(rest);
+console.log(first);
+console.log(rest);
 
-log.debug([first, ...rest]);
+console.log([first, ...rest]);
 
 Symbol.for("foo") === Symbol.for("foo"); // true
 
-log.debug("asd պօպօկ пндук".split(""));
+console.log("asd պօպօկ пндук".split(""));
